b021a0370152961c523dd45ea46fab46b59cdcfe
StoianBodurov <stoian.bodurov@abv.bg>
2022-08-17 11:22:27 +0300

    add AuthManger

---
+++ managers/auth.py
@@ -0,0 +1,36 @@
+from datetime import datetime, timedelta
+
+import jwt
+from decouple import config
+from flask_httpauth import HTTPTokenAuth
+from werkzeug.exceptions import Unauthorized
+
+
+class AuthManager:
+    @staticmethod
+    def encode_token(user):
+        payload = {
+            'sub': user.id,
+            'exp': datetime.utcnow() + timedelta(hours=2)
+        }
+        return jwt.encode(payload, key=config("SECRET_KEY"), algorithm='HS256')
+
+    @staticmethod
+    def decode_token(token):
+        try:
+            data = jwt.decode(jwt=token, key=config("SECRET_KEY"), algorithms=['HS256'])
+            return data['sub']
+        except Exception as ex:
+            raise ex
+
+
+auth = HTTPTokenAuth(scheme='Bearer')
+
+
+def verify_token(token):
+    try:
+        user_id = AuthManager.decode_token(token)
+        return user_id
+    except Exception:
+        raise Unauthorized('Invalid or missing token')
+    
