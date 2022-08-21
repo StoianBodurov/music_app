04715b6f47ba201166cea73ec55cb3f3d5e25c96
StoianBodurov <stoian.bodurov@abv.bg>
2022-08-21 11:05:19 +0300

    refactor permission_required decorator

--- utils/decorators.py
+++ utils/decorators.py
@@ -22,11 +22,12 @@ def validate_schema(schema_name):
 
 
 def permission_required(role):
-    def decorated_function(func):
+    def decorated(function):
+        @wraps(function)
         def wrapper(*args, **kwargs):
             current_user = auth.current_user()
             if not current_user.role == role:
                 raise Forbidden("You don't have permissions ")
-            return func(*args, **kwargs)
+            return function(*args, **kwargs)
         return wrapper
-    return decorated_function
+    return decorated
